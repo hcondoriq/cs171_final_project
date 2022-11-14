524b727f4b6dae3070c8bb55ae9b578a56b82a31
mayowaj <mayowa_jolayemi@college.harvard.edu>
2022-11-14 02:29:06 -0500

    MJ

--- js/circleTimeline.js
+++ js/circleTimeline.js
@@ -1,10 +1,11 @@
 class VisOne {
 
-    constructor(parentElement, data) {
+    constructor(parentElement, firstArray, secondArray, thirdArray) {
         this.parentElement = parentElement;
-        this.data = data;
-        // this.data2 = data2;
-        // this.filteredData = this.data;
+        this.firstArray = firstArray;
+        this.secondgroup = secondArray;
+        this.thirdgroup = thirdArray;
+        // this.filteredData =;
         this.displayData = []
 
         this.initvis()
@@ -18,6 +19,16 @@ class VisOne {
 
         let vis = this;
 
+        vis.sampleData = vis.firstArray.filter(function (d) {
+            // console.log("FILTER INFO", d)
+            if(d.basket_combined_simplified = "Fixed-broadband basket" && d.ITURegion == "Africa") {
+                return d[2008]
+            }
+            // return d.basket_current = "Fixed-broadband basket (5GB)"
+            // return d.Economy = "Aruba"
+        })
+        console.log("sample data", vis.sampleData)
+
         vis.margin = {top: 20, right: 20, bottom: 20, left: 20};
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
@@ -29,42 +40,52 @@ class VisOne {
             .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
 
         // create row group per family object
-        vis.rows = vis.svg.selectAll(".row-group")
-            .data(vis.data)
+        // vis.rows = vis.svg.selectAll(".row-group")
+        //     .data(vis.sampleData)
 
-        console.log("data??", vis.data)
-        console.log("rows?", vis.rows)
+        console.log("data??", vis.firstArray)
+        // console.log("rows?", vis.rows)
 
-        vis.row_groups = vis.rows
-            .enter()
-            .merge(vis.rows)
-            .append("g")
+        // vis.row_groups = vis.rows
+        //     .enter()
+        //     .merge(vis.rows)
+        //     .append("g")
 
-        // color scale
+        // color scale (to implement later)
         vis.colorScale
         // .range(["#FFFFFF", "#136D70"])
         // .domain([0, d3.max(vis.data2, d => d.internetcost)])
 
         // draw circles
-        vis.areaCell = vis.row_groups.selectAll()
-            .data(vis.data)
-            .enter().append("path")
-            .attr("r", d => d[2008])
-            .attr("cx", 0)
-            .attr("cy", 0)
-            .attr("fill", "red")
-        vis.areaCell1 = vis.row_groups.selectAll().data(vis.data1)
-            .enter().append("path")
-            .attr("r", d => d[2018])
-            .attr("cx", 0)
-            .attr("cy", 0)
-            .attr("fill", "red")
-        vis.areaCell2 = vis.row_groups.selectAll().data(vis.data1)
-            .enter().append("path")
-            .attr("r", d => d[2021])
-            .attr("cx", 0)
-            .attr("cy", 0)
-            .attr("fill", "red")
+        vis.areaCell = vis.svg.selectAll("circle")
+            .data(vis.sampleData)
+            .enter().append("circle")
+            .attr("r", function(d) {
+                if(d[2008] > 1000) {
+                    return d[2008] * .01
+                }
+                else if(200 < d[2008 < 1000 ]) {
+                    return d[2008] * 0.01
+                }
+                return d[2008] * .05
+            })
+            .attr("cx", (d,i) => {
+                return 5 + i * 19
+            })
+            .attr("cy", 30)
+            .attr("fill", "blue")
+        // vis.areaCell1 = vis.row_groups.selectAll().data(vis.data1)
+        //     .enter().append("path")
+        //     .attr("r", d => d[2018])
+        //     .attr("cx", 0)
+        //     .attr("cy", 0)
+        //     .attr("fill", "red")
+        // vis.areaCell2 = vis.row_groups.selectAll().data(vis.data1)
+        //     .enter().append("path")
+        //     .attr("r", d => d[2021])
+        //     .attr("cx", 0)
+        //     .attr("cy", 0)
+        //     .attr("fill", "red")
 
     }
 
@@ -73,7 +94,16 @@ class VisOne {
      * Data wrangling
      */
 
-    wrangleData() {}
+    wrangleData() {
+        // vis.sampleData = vis.firstArray.filter(function (d) {
+        //     // console.log("FILTER INFO", d)
+        //     if(d.basket_combined_simplified = "Fixed-broadband basket" && d.ITURegion == "Africa") {
+        //         return d[2008]
+        //     }
+        //     // return d.basket_current = "Fixed-broadband basket (5GB)"
+        //     // return d.Economy = "Aruba"
+        // })
+    }
 
 
     /*
--- js/circleTimelineMain.js
+++ js/circleTimelineMain.js
@@ -34,12 +34,70 @@ function updateAllVisualizations(){
 
 // load data using promises
 let promises = [
-    d3.csv("data/ICT_PriceBasket1.csv", row => {
-        // row.Wealth = +row.Wealth
-        // row.Priorates = +row.Priorates
+    d3.csv("data/ICT_PriceBasket1.csv", (row) => {
+        // console.log("what's row of 2008", row[2008])
+
+        if(isNaN(row[2008]) == true) {
+            row[2008] = 0
+        }
+        if(isNaN(row[2009]) == true) {
+            row[2009] = 0
+        }
+        if(isNaN(row[2010]) == true) {
+            row[2010] = 0
+        }
+        if(isNaN(row[2011]) == true) {
+            row[2011] = 0
+        }
+        if(isNaN(row[2012]) == true) {
+            row[2012] = 0
+        }
+        if(isNaN(row[2013]) == true) {
+            row[2013] = 0
+        }
+        if(isNaN(row[2014]) == true) {
+            row[2014] = 0
+        }
+        if(isNaN(row[2015]) == true) {
+            row[2015] = 0
+        }
+        if(isNaN(row[2016]) == true) {
+            row[2016] = 0
+        }
+        if(isNaN(row[2017]) == true) {
+            row[2017] = 0
+        }
+        if(isNaN(row[2018]) == true) {
+            row[2018] = 0
+        }
+        if(isNaN(row[2019]) == true) {
+            row[2019] = 0
+        }
+        if(isNaN(row[2020]) == true) {
+            row[2020] = 0
+        }
+        if(isNaN(row[2021]) == true) {
+            row[2021] = 0
+        }
+        // delete row.basket_current = "Fixed-broadband basket (5GB)"
+
+        row[2008] = +row[2008]
+        row[2009] = +row[2009]
+        row[2010] = +row[2010]
+        row[2011] = +row[2011]
+        row[2012] = +row[2012]
+        row[2013] = +row[2013]
+        row[2014] = +row[2014]
+        row[2015] = +row[2015]
+        row[2016] = +row[2016]
+        row[2017] = +row[2017]
+        row[2018] = +row[2018]
+        row[2019] = +row[2019]
+        row[2020] = +row[2020]
+        row[2021] = +row[2021]
         return row
     }),
-    d3.csv("data/ICT_PriceBasket1-5.csv", row => {
+    d3.csv("data/ICT_PriceBaskets1-5.csv", row => {
         row[2008] = +row[2008]
         row[2009] = +row[2009]
         row[2010] = +row[2010]
@@ -88,9 +146,22 @@ let promises = [
         row[2021] = +row[2021]
         return row}),
     d3.csv("data/ICT_PriceBasket3.csv", row => {
-        return row
+        row[2008] = +row[2008]
+        row[2009] = +row[2009]
+        row[2010] = +row[2010]
+        row[2011] = +row[2011]
+        row[2012] = +row[2012]
+        row[2013] = +row[2013]
+        row[2014] = +row[2014]
+        row[2015] = +row[2015]
+        row[2016] = +row[2016]
+        row[2017] = +row[2017]
+        row[2018] = +row[2018]
+        row[2019] = +row[2019]
+        row[2020] = +row[2020]
+        row[2021] = +row[2021]
     }),
-    d3.csv("data/ICT_PriceBasket3-5.csv", row => {
+    d3.csv("data/ICT_PriceBaskets3-5.csv", row => {
         row[2008] = +row[2008]
         row[2009] = +row[2009]
         row[2010] = +row[2010]
