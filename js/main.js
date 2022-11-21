ecd396df6c1b809148215257e33ac540c4a144b8
Dagmawit Adamu <dadamu@college.harvard.edu>
2022-11-20 22:34:19 -0500

    added map and timeline w/ brushing

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 * * * * * * * * * * * * * */
 
 // init global variables,  helper functions
-let myMapBrushVis;
+let myMapVis;
+let myTimelineBrushVis;
 
 function updateAllVisualizations() {
     visEduc.wrangleData()
@@ -12,17 +13,19 @@ function updateAllVisualizations() {
 // load data using promises
 let promises = [
     d3.csv("data/fixed-broadband-subscriptions.csv"), // broadbandData
-    d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json") // geoData
+    d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json"), // geoData
+    d3.json("data/world-broadband.json"), // worldBroadbandData
 ];
 
 Promise.all(promises)
     .then(function (data) {
-        console.log("check out the data", data)
+        console.log("check out the data", data);
 
-        data[0].forEach(function(d){
-            console.log(d["Country Name"]);
+        // convert the years in worldBroadbandData to dates
+        let parseDate = d3.timeParse("%Y");
+        data[2].years.forEach(function(d){
+            d["Year"] = parseDate(d["Year"].toString());
         });
-
 
         initMainPage(data)
     })
@@ -34,6 +37,44 @@ function initMainPage(allDataArray) {
     // log data
     console.log(allDataArray);
 
-    myMapBrushVis = new MapBrushVis('mapBrushDiv', allDataArray[0], allDataArray[1])
+    // create event handler
+    let eventHandler = {
+        bind: (eventName, handler) => {
+            document.body.addEventListener(eventName, handler);
+        },
+        trigger: (eventName, extraParameters) => {
+            document.body.dispatchEvent(new CustomEvent(eventName, {
+                detail: extraParameters
+            }));
+        }
+    }
+
+    // create instances
+    myMapVis = new MapVis('mapDiv', allDataArray[0], allDataArray[1])
+    myTimelineBrushVis = new TimelineBrushVis('timelineBrushDiv', allDataArray[2].years, eventHandler)
+
+    // bind event handler
+    eventHandler.bind("selectionChanged", function(event){
+        let rangeStart = event.detail[0];
+        // console.log("start range", rangeStart)
+        let rangeEnd = event.detail[1];
+        // console.log("end range", rangeEnd)
+        myMapVis.onSelectionChange(rangeStart, rangeEnd);
+    });
+
+    // function brushed() {
+    //
+    //     // TO-DO: React to 'brushed' event
+    //     // Get the extent of the current brush
+    //     let selectionRange = d3.brushSelection(d3.select(".brush").node());
+    //     console.log("selection range", selectionRange)
+    //
+    //     // Convert the extent into the corresponding domain values
+    //     // let selectionDomain = selectionRange.map(timeline.x.invert);
+    //     // myMapVis.x.domain(selectionDomain);
+    //     //
+    //     // myMapVis.wrangleData();
+    //
+    // }
 
 }
---
+++ js/timelineBrushVis.js
@@ -0,0 +1,132 @@
+/*
+ * TimelineBrushVis - Object constructor function
+ * @param _parentElement 	-- the HTML element in which to draw the timeline brushing tool
+ * @param _worldBroadbandData	-- the json data file 'world-broadband.json'
+ */
+
+
+class TimelineBrushVis {
+
+    constructor(parentElement, worldBroadbandData, eventHandler) {
+        this.parentElement = parentElement;
+        this.worldBroadbandData = worldBroadbandData;
+        this.eventHandler = eventHandler;
+
+        this.initVis()
+    }
+
+    initVis() {
+        let vis = this;
+
+        vis.margin = {top: 0, right: 40, bottom: 30, left: 40};
+
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height  - vis.margin.top - vis.margin.bottom;
+
+        // SVG drawing area
+        vis.svg = d3.select("#" + vis.parentElement).append("svg")
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+        // add title
+        vis.svg.append('g')
+            .attr('class', 'title')
+            .attr('id', 'timeline-brush-title')
+            .append('text')
+            .text('How has worldwide access to broadband internet changed over time?')
+            .attr('transform', `translate(${vis.width / 2}, 20)`)
+            .attr('text-anchor', 'middle');
+
+
+        // scales and axes
+        vis.x = d3.scaleTime()
+            .range([0, vis.width])
+            .domain(d3.extent(vis.worldBroadbandData, function(d) { return d["Year"]; }));
+
+        vis.y = d3.scaleLinear()
+            .range([vis.height, 0])
+            .domain([0, d3.max(vis.worldBroadbandData, function(d) { return d["Fixed Broadband Subscriptions"]; })]);
+
+        vis.xAxis = d3.axisBottom()
+            .scale(vis.x)
+
+        vis.yAxis = d3.axisLeft()
+            .scale(vis.y);
+
+
+        // define area path generator
+        vis.area = d3.area()
+            .x(function(d) { return vis.x(d["Year"]); })
+            .y0(vis.height)
+            .y1(function(d) { return vis.y(d["Fixed Broadband Subscriptions"]); });
+
+        // draw area by using the path generator
+        vis.svg.append("path")
+            .datum(vis.worldBroadbandData)
+            .attr("class", "area")
+            .attr("fill", "#CBC3E3")
+            .attr("d", vis.area);
+
+        // append x-axis
+        vis.svg.append("g")
+            .attr("class", "x-axis axis")
+            .attr("transform", "translate(0," + vis.height + ")")
+            .call(vis.xAxis);
+
+        // append y-axis
+        vis.svg.append("g")
+            .attr("class", "y-axis axis")
+            .call(vis.yAxis);
+
+        // append y-axis title
+        vis.svg.append("text")
+            .attr("x", 10)
+            .attr("y", 15)
+            .attr("class", "timeline-axis-title")
+            .text("Fixed Broadband Subscriptions (per 100 people)");
+
+        // initialize brushing component
+        vis.brush = d3.brushX()
+            .extent([[0, 0], [vis.width, vis.height]])
+            // .on("brush", brushed);
+            .on("brush", function(event){
+                // user selected a specific region
+                vis.currentBrushRegion = event.selection;
+                console.log("current brush reg", vis.currentBrushRegion)
+
+                vis.currentBrushRegion = vis.currentBrushRegion.map(vis.x.invert);
+
+                // // trigger the event 'selectionChanged' of our event handler
+                vis.eventHandler.trigger("selectionChanged", vis.currentBrushRegion);
+            });
+
+        // append brush component
+        vis.brushGroup = vis.svg.append("g")
+            .attr("class", "brush")
+
+
+        vis.wrangleData();
+    }
+
+    wrangleData() {
+        let vis = this;
+
+        console.log(vis.worldBroadbandData)
+        // vis.worldBroadbandData.forEach(d => {
+        //     console.log(d)
+        // })
+
+        vis.updateVis();
+    }
+
+    updateVis() {
+        let vis = this;
+
+        // call brush component
+        vis.brushGroup.call(vis.brush)
+
+    }
+
+}
