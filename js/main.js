c553242f1ff53d91b0a0ab5780c99ff367fef8a3
mayowaj <mayowa_jolayemi@college.harvard.edu>
2022-11-21 00:14:54 -0500

    MJ

--- js/main.js
+++ js/main.js
@@ -28,7 +28,122 @@ let promises = [
     d3.csv("data/educ_att_sec_aggreg.csv"),
     d3.csv("data/fixed-broadband-subscriptions.csv"), // broadbandData
     d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json"), // geoData
-    d3.json("data/world-broadband.json"), // worldBroadbandData
+    d3.json("data/world-broadband.json"), // worldBroadbandData,
+    d3.csv("data/ICTMedianBaskets.csv", row => {
+        // console.log("median baskets", row)
+        if(isNaN(row[2008]) == true) {
+            row[2008] = 0
+        }
+        if(isNaN(row[2009]) == true) {
+            row[2009] = 0
+        }
+        if(isNaN(row[2010]) == true) {
+            row[2010] = 0
+        }
+        if(isNaN(row[2011]) == true) {
+            row[2011] = 0
+        }
+        if(isNaN(row[2012]) == true) {
+            row[2012] = 0
+        }
+        if(isNaN(row[2013]) == true) {
+            row[2013] = 0
+        }
+        if(isNaN(row[2014]) == true) {
+            row[2014] = 0
+        }
+        if(isNaN(row[2015]) == true) {
+            row[2015] = 0
+        }
+        if(isNaN(row[2016]) == true) {
+            row[2016] = 0
+        }
+        if(isNaN(row[2017]) == true) {
+            row[2017] = 0
+        }
+        if(isNaN(row[2018]) == true) {
+            row[2018] = 0
+        }
+        if(isNaN(row[2019]) == true) {
+            row[2019] = 0
+        }
+        if(isNaN(row[2020]) == true) {
+            row[2020] = 0
+        }
+        if(isNaN(row[2021]) == true) {
+            row[2021] = 0
+        }
+
+        row[2008] = +row[2008]
+        row[2009] = +row[2009]
+        row[2010] = +row[2010]
+        row[2011] = +row[2011]
+        row[2012] = +row[2012]
+        row[2013] = +row[2013]
+        row[2014] = +row[2014]
+        row[2015] = +row[2015]
+        row[2016] = +row[2016]
+        row[2017] = +row[2017]
+        row[2018] = +row[2018]
+        row[2019] = +row[2019]
+        row[2020] = +row[2020]
+        row[2021] = +row[2021]
+
+        row[0] = +row[0]
+        row[1] = +row[1]
+        row[2] = +row[2]
+        row[3] = +row[3]
+        row[4] = +row[4]
+        row[5] = +row[5]
+        row[6] = +row[6]
+        row[7] = +row[7]
+        row[8] = +row[8]
+        row[9] = +row[9]
+        row[10] = +row[10]
+        row[11] = +row[11]
+        row[12] = +row[12]
+
+
+
+
+
+        return row
+    }),
+    d3.csv("data/InternetUse_Region.csv", row => {
+        row[1990] = +row[1990]
+        row[1991] = +row[1991]
+        row[1992] = +row[1992]
+        row[1993] = +row[1993]
+        row[1994] = +row[1994]
+        row[1995] = +row[1995]
+        row[1996] = +row[1996]
+        row[1997] = +row[1997]
+        row[1998] = +row[1998]
+        row[1999] = +row[1999]
+        row[2000] = +row[2000]
+        row[2001] = +row[2001]
+        row[2002] = +row[2002]
+        row[2003] = +row[2003]
+        row[2004] = +row[2004]
+        row[2005] = +row[2005]
+        row[2006] = +row[2006]
+        row[2007] = +row[2007]
+        row[2008] = +row[2008]
+        row[2009] = +row[2009]
+        row[2010] = +row[2010]
+        row[2011] = +row[2011]
+        row[2012] = +row[2012]
+        row[2013] = +row[2013]
+        row[2014] = +row[2014]
+        row[2015] = +row[2015]
+        row[2016] = +row[2016]
+        row[2017] = +row[2017]
+        row[2018] = +row[2018]
+        row[2019] = +row[2019]
+        row[2020] = +row[2020]
+        row[2021] = +row[2021]
+        return row
+    }),
 ];
 let data_glob
 Promise.all(promises)
@@ -55,6 +170,32 @@ function initMainPage(allDataArray) {
 
     myvisEduc = new visEduc('visual_educ', allDataArray[0], allDataArray[1])
     init();
+
+    // create event handler
+    let eventHandler = {
+        bind: (eventName, handler) => {
+            document.body.addEventListener(eventName, handler);
+        },
+        trigger: (eventName, extraParameters) => {
+            document.body.dispatchEvent(new CustomEvent(eventName, {
+                detail: extraParameters
+            }));
+        }
+    }
+
+    // create instances
+    myMapVis = new MapVis('mapDiv', allDataArray[2], allDataArray[3])
+    myTimelineBrushVis = new TimelineBrushVis('timelineBrushDiv', allDataArray[4].years, eventHandler)
+    myVisOne = new VisOne('timeline', allDataArray[5], allDataArray[6])
+
+    // bind event handler
+    eventHandler.bind("selectionChanged", function(event){
+        let rangeStart = event.detail[0];
+        // console.log("start range", rangeStart)
+        let rangeEnd = event.detail[1];
+        // console.log("end range", rangeEnd)
+        myMapVis.onSelectionChange(rangeStart, rangeEnd);
+    });
     // kick things off
 
 }
@@ -92,32 +233,6 @@ const stepEvents = {
                 .style("visibility", "hidden")
                 .transition()
                 .duration(1000)
-
-
-    // create event handler
-    let eventHandler = {
-        bind: (eventName, handler) => {
-            document.body.addEventListener(eventName, handler);
-        },
-        trigger: (eventName, extraParameters) => {
-            document.body.dispatchEvent(new CustomEvent(eventName, {
-                detail: extraParameters
-            }));
-        }
-    }
-
-    // create instances
-    myMapVis = new MapVis('mapDiv', allDataArray[2], allDataArray[3])
-    myTimelineBrushVis = new TimelineBrushVis('timelineBrushDiv', allDataArray[4].years, eventHandler)
-
-    // bind event handler
-    eventHandler.bind("selectionChanged", function(event){
-        let rangeStart = event.detail[0];
-        // console.log("start range", rangeStart)
-        let rangeEnd = event.detail[1];
-        // console.log("end range", rangeEnd)
-        myMapVis.onSelectionChange(rangeStart, rangeEnd);
-    });
 
         }
     }
