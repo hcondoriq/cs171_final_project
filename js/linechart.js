16b7428fd0c156327d69ef3ba994aaa039086f9f
mayowaj <mayowa_jolayemi@college.harvard.edu>
2022-12-05 10:53:00 -0500

    MJ

--- js/linechart.js
+++ js/linechart.js
@@ -18,7 +18,7 @@ class LineVis {
     initVis() {
         let vis = this;
 
-        vis.margin = {top: 20, right: 40, bottom: 20, left: 10};
+        vis.margin = {top: 20, right: 40, bottom: 20, left: 30};
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
